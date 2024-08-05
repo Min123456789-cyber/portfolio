ddeeff14f221ae26a58d44531823b6d5a1a8ac3c
Min Bahadur Gurung <meen.gurung333@gmail.com>
2024-08-05 20:54:31 +0545

    back to top

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,14 +1,27 @@
-import React from 'react'
-import LogoPic from './Logo'
+import React from "react";
+import LogoPic from "./Logo";
+import { IoIosArrowUp } from "react-icons/io";
 
 const Footer = () => {
-  return (
-    <footer className='d-flex flex-column justify-content-center align-items-center bg-body-tertiary'>
-      <LogoPic/>
-      <h3 className='text-xl text-center text-dark pb-3'>Copyrights &copy; 2024 
-      </h3>
-    </footer>
-  )
-}
+  const top = () => {
+    window.scrollTo(0, 0);
+  };
 
-export default Footer
+  return (
+    <footer>
+        <IoIosArrowUp
+          onClick={() => top()}
+          className="fs-1 position-absolute bg-dark bg-gradient rounded text-white p-1"
+          style={{ right: "0" }}
+        />
+      <div className="d-flex flex-column justify-content-center align-items-center bg-body-tertiary">
+        <LogoPic />
+        <h3 className="text-xl text-center text-dark pb-3">
+          Copyrights &copy; 2024
+        </h3>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 
 const Logo = () => {
   return (
-    <div className="p-3 cursor-pointer border-none">
+    <div className="p-3 border-none">
       <Link to="/">
         <img src={LogoPic} style={{width: "50px", height: "50px"}} alt="Logo" />
       </Link>
